7451e96a920565869ac432b813e15fff4813f5dd
Greg Thompson <thompsgj@yahoo.com>
2017-07-16 23:21:37 +0900

    Data moved from views to controllers.  Intial mongodb setup.

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /*GET About Page */
 module.exports.about = function(req,res) {
-  res.render('generic-text', {title:'About'});
+  res.render('generic-text', {
+  	title:'About Loc8r',
+  	content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, integer in nunc laoreet id pharetra ut. In integer, ultrices morbi malesuada per, faucibus gravida eu suspendisse erat enim ac, imperdiet ultricies posuere suspendisse morbi dui sociosqu, eu orci morbi a. Sodales mauris augue pulvinar mauris, nunc nulla elit in integer, scelerisque odio eleifend volutpat, purus lorem fusce viverra hymenaeos. Vel venenatis convallis id elit nonummy. Mauris felis sed nunc id, dolor euismod vestibulum tristique dignissim.\n\nPharetra libero sit. Quisque molestie risus suscipit turpis, ultricies luctus commodo metus libero proin, amet sem nunc tortor neque, magna id. Lacus purus, tempus odio arcu sed, duis in et ipsam risus justo, mauris orci dolor nisl. Orci dignissim mi nibh ultrices cras, primis eget magnis sollicitudin odio dignissim aliquam, erat exercitation, montes et posuere eget nec. Iaculis eleifend, pretium ante leo dis ligula sit. Eget at, eu dictum, sit sapien arcu bibendum necessitatibus. Aliquet sodales donec ipsum dolor, et gravida eu sit curabitur, nam dictum eu lacinia libero arcu. Risus senectus at vel, aenean integer urna nibh.'
+  });
 };
---
+++ app_server/models/db.js
@@ -0,0 +1,57 @@
+var mongoose = require('mongoose');
+var gracefulShutdown;
+var dbURI = 'mongodb://localhost/Loc8r';
+mongoose.connect(dbURI);
+
+
+var readLine = require('readline');
+if(process.platform === "win32") {
+	var rl = readLine.createInterface ({
+		input:process.stdin,
+		output:process.stdout
+	});
+	rl.on("SIGINT", function() {
+		process.emit("SIGINT");
+	});
+}
+
+
+//Activity Checks
+mongoose.connection.on('connected', function(){
+	console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', function(err){
+	console.log('Mongoose connected to ' + err);
+});
+
+mongoose.connection.on('disconnected', function(){
+	console.log('Mongoose disconnected');
+});
+
+
+gracefulShutdown = function(msg, callback) {
+	mongoose.connection.close(function(){
+		console.log('Mongoose disconnected through ' + msg);
+		callback();
+	});
+};
+
+//Nodemon Restarts
+process.once('SIGUSR2', function(){
+	gracefulShutdown('nodemon restart', function() {
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+//App Termination
+process.on('SIGINT', function(){
+	gracefulShutdown('app termination', function(){
+		process.exit(0);
+	});
+});
+//Heroku App Termination
+process.on('SIGTERM', function(){
+	gracefulShutdown('Heroku app shutdown', function() {
+		process.exit(0);
+	});
+});
